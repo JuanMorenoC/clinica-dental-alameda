23e9e567fc8759d3af626a3854006ef529c2a6b4
Juan <jdmorenoc@uqvirtual.edu.co>
2021-05-11 16:56:48 -0500

    Agenda por Juan 11 mayo parte 2

--- src/app/agenda/agenda.component.ts
+++ src/app/agenda/agenda.component.ts
@@ -14,29 +14,7 @@ export class AgendaComponent implements OnInit {
   cargarData(): void{
     this.agendaService.getAgenda().subscribe((data: any) => {
       console.log(data);
-      // console.log(data);
-      this.listaAgenda = data;
     });
-  }
-
-  obtenerColor(data: EventInput, n: number): string {
-    let color = '';
-    if (data[n].estado === 'Sala espera'){
-      color = 'yellow';
-    }
-    if (data[n].estado === 'Cancelado'){
-      color = 'gray';
-    }
-    if (data[n].estado === 'Pendiente'){
-      color = 'red';
-    }
-    if (data[n].estado === 'Confirmado'){
-      color = 'purple';
-    }
-    if (data[n].estado === 'Esta en el Box'){
-      color = 'green';
-    }
-    return color;
   }
 }
 
