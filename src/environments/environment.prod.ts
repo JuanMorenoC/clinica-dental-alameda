f393e3fd564fdac2175f45580dd4ad21861bd98b
Juan <jdmorenoc@uqvirtual.edu.co>
2021-08-18 12:03:02 -0500

    Cambio Color y Letra del Navbar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  productionUrl: 'http://clinicaalameda-env.eba-nw5jhjfw.us-east-2.elasticbeanstalk.com',
+  // productionUrl: 'http://clinicaalameda-env.eba-nw5jhjfw.us-east-2.elasticbeanstalk.com',
+  productionUrl: 'http://localhost:8082',
   productionPatch: '/api'
 };
