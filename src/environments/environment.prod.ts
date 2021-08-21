608cec872d900c851790913eca1a334e65145db8
Juan <jdmorenoc@uqvirtual.edu.co>
2021-08-21 11:14:11 -0500

    Configurando para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // productionUrl: 'http://clinicaalameda-env.eba-nw5jhjfw.us-east-2.elasticbeanstalk.com',
-  productionUrl: 'http://localhost:8082',
-  productionPatch: '/api'
+  productionUrl: 'https://clinica-alameda-backend.herokuapp.com/',
+  // productionUrl: 'http://localhost:8082',
+  productionPatch: 'api'
 };
